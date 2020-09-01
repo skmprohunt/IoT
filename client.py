f7a6b6d67c0d3a96734216170eccd0e0733cddfe
Saurabh Mishra <35428409+skmprohunt@users.noreply.github.com>
2020-09-01 16:33:58 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,12 +1,8 @@
-#!/usr/bin/python           # This is client.py file
-
-import socket               # Import socket module
-
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 12345                # Reserve a port for your service.
-
+import socket               
+s = socket.socket()         
+host = socket.gethostname() 
+port = 12345                
 s.connect((host, port))
 print(s.recv(1024))
 s.send(b"78781F100F0C1D0B0F34C6027AC74C0C4658100014D401CC00287D001F71002623090D0A")
-s.close()                     # Close the socket when done
+s.close()                     
